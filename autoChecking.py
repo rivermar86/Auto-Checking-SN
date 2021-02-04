64483650f589174c2d5a83cf4e8ee831c01c4028
Marco_Razo <rivermar86@gmail.com>
2021-02-04 02:03:59 -0600

    Latest stable version

--- autoChecking.py
+++ autoChecking.py
@@ -9,36 +9,36 @@ from tkinter import ttk
 import tkinter.messagebox 
 
 myWin = Tk()
-textArea = Text(myWin, width='18', height='11')
+textArea = Text(myWin, width = '18', height = '11')
 driver = webdriver.Chrome('C:\\Users\\MarcoA_Rivera.FOX-GDL\\Desktop\\Python'\
                             '\\AutomateCheking\\chromedriver.exe')
 #driver = webdriver.Chrome('.exe\\selenium\\webdriver')
-imgtitle = PhotoImage(file=r"C:/Users/MarcoA_Rivera.FOX-GDL/Desktop/Python/"\
+imgtitle = PhotoImage(file = r"C:/Users/MarcoA_Rivera.FOX-GDL/Desktop/Python/"\
                            r"AutomateCheking/Title.gif")
 #imgtitle = PhotoImage(file=r".gif\\Title.gif")
 
 def theWindow():
     global lstBox, textArea
     myWin.geometry('376x380')
-    textArea.config(font=('Consolas',17))
-    btnDone = Button(myWin, text='Process', command=autoChecking)
+    textArea.config(font = ('Consolas', 17))
+    btnDone = Button(myWin, text = 'Process', command = autoChecking)
    
-    imgFrame = Label(myWin,image=imgtitle)
-    labQty = Label(myWin,text='Qty:')
-    lstBox = ttk.Combobox(myWin, width=10,height=12,state='readonly')
+    imgFrame = Label(myWin, image = imgtitle)
+    labQty = Label(myWin, text = 'Qty:')
+    lstBox = ttk.Combobox(myWin, width = 10, height = 12, state = 'readonly')
     lstBox.set('1')
-    lstBox['value'] = ['1','2','3','4','5','6','7','8','9','10']
+    lstBox['value'] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
     lstBox.bind('<<ComboboxSelected>>', selection_changed)
      
-    imgFrame.place(x=1,y=1) 
-    labQty.place(x=253,y=70)
-    textArea.place(x=4,y=70)
-    lstBox.place(x=283,y=70)
-    btnDone.place(x=283,y=98)
+    imgFrame.place(x = 1, y = 1) 
+    labQty.place(x = 253, y = 70)
+    textArea.place(x = 4, y = 70)
+    lstBox.place(x = 283, y = 70)
+    btnDone.place(x = 283, y = 98)
     return myWin
 
 def selection_changed(event):
-    textArea.delete(1.0,END)
+    textArea.delete(1.0, END)
     textArea.focus()
     
 def switch_frame():
@@ -73,18 +73,18 @@ def autoChecking():
     driver.switch_to_frame(driver.find_element_by_xpath("/html/body/table"\
                            "/tbody/tr/td[2]/iframe"))
 
-    x=driver.find_element_by_xpath("/html/body/div[2]/div[1]/a[2]")
+    x = driver.find_element_by_xpath("/html/body/div[2]/div[1]/a[2]")
     x.click()
     
-    y=driver.find_element_by_id('node2101005')
+    y = driver.find_element_by_id('node2101005')
     y.click()
 
     #return again to switch to another frame
     switch_frame()
     driver.switch_to_frame(driver.find_element_by_xpath(
                            '/html/frameset/frame[1]'))  
-    tag=[]
-    screen =[]
+    tag = []
+    screen = []
     
     for x in range(len(dataList)):
         #type SerialNumber
